68ff825e79d67856aeaa9b1159dc2f318180407c
GreenBeaver <leonkhvatskin@gmail.com>
2019-10-26 22:49:41 +0300

    smth finally works

--- src/main/resources/js/views/hot.js
+++ src/main/resources/js/views/hot.js
@@ -1,21 +1,9 @@
-requirejs.config({
-    baseUrl: 'js'
-});
-
-require(
+define(
     ['views/article'],
     function (article) {
-        webix.protoUI({
-            name: "article",
-            $init:function(config){
-                this.set(article(config.rating, config.author, config.date))
-            }
-        }, webix.ui.layout);
-
         return {
             rows: [
-                {view: "article", rating: 12, author: "sdfsdf", date: "sgsdf"}
+                article
             ]
         }
-    }
-);
+    });
