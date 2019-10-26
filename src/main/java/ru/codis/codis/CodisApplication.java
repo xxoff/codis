5c86ca69d3441ebab93510415aeedc1dbc345e1a
NastyPill <DJPonnYash.Vinyl@gmail.com>
2019-10-26 17:14:22 +0700

    initial commit

---
+++ src/main/java/ru/codis/codis/CodisApplication.java
@@ -0,0 +1,13 @@
+package ru.codis.codis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/codis/codis/CodisApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.codis.codis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
