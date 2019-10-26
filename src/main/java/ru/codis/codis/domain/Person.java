e1d89ac5b299913529c84e9110f0dd87a350a314
NastyPill <DJPonnYash.Vinyl@gmail.com>
2019-10-26 19:09:46 +0700

    crudRepo done

---
+++ src/main/java/ru/codis/codis/domain/Person.java
@@ -0,0 +1,18 @@
+package ru.codis.codis.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+
+    private String name;
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,27 @@
+requirejs.config({
+    baseUrl: 'js'
+});
+
+function buildRoute(view, id) {
+    return function () {
+        webix.ui({
+            id: id,
+            rows: [
+                view
+            ]
+        }, $$(id));
+    }
+}
+
+require(
+    [],
+    function () {
+        webix.ready(function () {
+            webix.ui({
+                template: "Bill"
+            })
+        });
+
+        routie({
+        })
+    });
