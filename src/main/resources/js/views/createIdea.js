66059d9cac353c20524252d50c0850df648f70d4
Timofey <nikiforov.ta@edu.spbstu.ru>
2019-10-27 05:14:08 +0300

    add form for creation of an idea

--- src/main/resources/js/views/createIdea.js
+++ src/main/resources/js/views/createIdea.js
@@ -1,8 +1,47 @@
 define(function () {
     return {
         rows: [
-            {template: "asd"},
-            {template: "asd"}
+            {
+                label: "Create idea",
+                view: "label",
+                width: 500,
+                name: "label",
+                align: "center"
+            },
+            {
+                view: "text",
+                width: 500,
+                placeholder: "Header (name)"
+            },
+            {
+                view: "textarea",
+                width: 500,
+                height: 60,
+                placeholder: "Description"
+            },
+            {
+                cols: [{
+                    view: "counter",
+                    width: 200,
+                    value: 1,
+                    label: "Difficulty",
+                    min: 1,
+                    max: 5,
+                    step: 0.5
+                },
+                    {
+                        view: "text",
+                        width: 300,
+                        placeholder: "Author"
+                    }
+                ]
+            },
+            {
+                label: "Create",
+                type: "form",
+                view: "button",
+                width: 500
+            }
         ]
     }
 });
